21e9bc217016dfc8bf2afb9bdedb55085b8b6246
nickdobson <nick.dobson@gmail.com>
2023-07-19 15:16:05 -0400

    updated build to 38.1.1

--- packages/ckeditor5-build-classic/build/ckeditor.d.ts
+++ packages/ckeditor5-build-classic/build/ckeditor.d.ts
@@ -6,24 +6,37 @@ import { ClassicEditor as ClassicEditorBase } from '@ckeditor/ckeditor5-editor-c
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { UploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder';
 import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import { Bold, Code, Italic, Strikethrough, Subscript, Superscript, Underline } from '@ckeditor/ckeditor5-basic-styles';
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
 import { CKBox } from '@ckeditor/ckeditor5-ckbox';
 import { CKFinder } from '@ckeditor/ckeditor5-ckfinder';
 import { EasyImage } from '@ckeditor/ckeditor5-easy-image';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload, PictureEditing } from '@ckeditor/ckeditor5-image';
-import { Indent } from '@ckeditor/ckeditor5-indent';
-import { Link } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Heading, Title } from '@ckeditor/ckeditor5-heading';
+import { AutoImage, Image, ImageCaption, ImageInsert, ImageResize, ImageStyle, ImageToolbar, ImageUpload, PictureEditing } from '@ckeditor/ckeditor5-image';
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
+import { Link, AutoLink, LinkImage } from '@ckeditor/ckeditor5-link';
+import { List, ListProperties, TodoList } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import { Table, TableCellProperties, TableProperties, TableToolbar } from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
+import { SpecialCharacters, SpecialCharactersArrows, SpecialCharactersCurrency, SpecialCharactersEssentials, SpecialCharactersLatin, SpecialCharactersMathematical, SpecialCharactersText } from '@ckeditor/ckeditor5-special-characters';
+import { WordCount } from '@ckeditor/ckeditor5-word-count';
+import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
+import { PageBreak } from '@ckeditor/ckeditor5-page-break';
+import { Mention } from '@ckeditor/ckeditor5-mention';
+import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import { Highlight } from '@ckeditor/ckeditor5-highlight';
+import { FontBackgroundColor, FontColor, FontFamily, FontSize } from '@ckeditor/ckeditor5-font';
+import { ExportPdf } from '@ckeditor/ckeditor5-export-pdf';
+import { ExportWord } from '@ckeditor/ckeditor5-export-word';
 export default class ClassicEditor extends ClassicEditorBase {
-    static builtinPlugins: (typeof TextTransformation | typeof Essentials | typeof UploadAdapter | typeof Paragraph | typeof Heading | typeof Autoformat | typeof Bold | typeof Italic | typeof BlockQuote | typeof CloudServices | typeof Image | typeof ImageCaption | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof CKBox | typeof CKFinder | typeof EasyImage | typeof Indent | typeof Link | typeof List | typeof MediaEmbed | typeof PasteFromOffice | typeof Table | typeof TableToolbar | typeof PictureEditing)[];
+    static builtinPlugins: (typeof TextTransformation | typeof Essentials | typeof UploadAdapter | typeof Paragraph | typeof Heading | typeof Title | typeof Autoformat | typeof Superscript | typeof Subscript | typeof Bold | typeof Code | typeof Italic | typeof Strikethrough | typeof Underline | typeof BlockQuote | typeof CloudServices | typeof AutoImage | typeof Image | typeof ImageCaption | typeof ImageInsert | typeof ImageResize | typeof ImageStyle | typeof ImageToolbar | typeof ImageUpload | typeof CKBox | typeof CKFinder | typeof EasyImage | typeof Indent | typeof IndentBlock | typeof AutoLink | typeof Link | typeof LinkImage | typeof List | typeof ListProperties | typeof TodoList | typeof MediaEmbed | typeof MediaEmbedToolbar | typeof PasteFromOffice | typeof Table | typeof TableCellProperties | typeof TableProperties | typeof TableToolbar | typeof Alignment | typeof CodeBlock | typeof SpecialCharacters | typeof SpecialCharactersText | typeof SpecialCharactersArrows | typeof SpecialCharactersEssentials | typeof SpecialCharactersLatin | typeof SpecialCharactersCurrency | typeof SpecialCharactersMathematical | typeof WordCount | typeof RemoveFormat | typeof PageBreak | typeof Mention | typeof HtmlEmbed | typeof HorizontalLine | typeof Highlight | typeof FontBackgroundColor | typeof FontColor | typeof FontFamily | typeof FontSize | typeof ExportPdf | typeof ExportWord | typeof PictureEditing)[];
     static defaultConfig: {
         toolbar: {
             items: string[];
--- packages/ckeditor5-build-classic/src/ckeditor.ts
+++ packages/ckeditor5-build-classic/src/ckeditor.ts
@@ -9,62 +9,176 @@ import { ClassicEditor as ClassicEditorBase } from '@ckeditor/ckeditor5-editor-c
 import { Essentials } from '@ckeditor/ckeditor5-essentials';
 import { UploadAdapter } from '@ckeditor/ckeditor5-adapter-ckfinder';
 import { Autoformat } from '@ckeditor/ckeditor5-autoformat';
-import { Bold, Italic } from '@ckeditor/ckeditor5-basic-styles';
+import {
+	Bold,
+	Code,
+	Italic,
+	Strikethrough,
+	Subscript,
+	Superscript,
+	Underline,
+} from '@ckeditor/ckeditor5-basic-styles';
 import { BlockQuote } from '@ckeditor/ckeditor5-block-quote';
 import { CKBox } from '@ckeditor/ckeditor5-ckbox';
 import { CKFinder } from '@ckeditor/ckeditor5-ckfinder';
 import { EasyImage } from '@ckeditor/ckeditor5-easy-image';
-import { Heading } from '@ckeditor/ckeditor5-heading';
-import { Image, ImageCaption, ImageStyle, ImageToolbar, ImageUpload, PictureEditing } from '@ckeditor/ckeditor5-image';
-import { Indent } from '@ckeditor/ckeditor5-indent';
-import { Link } from '@ckeditor/ckeditor5-link';
-import { List } from '@ckeditor/ckeditor5-list';
-import { MediaEmbed } from '@ckeditor/ckeditor5-media-embed';
+import { Heading, Title } from '@ckeditor/ckeditor5-heading';
+import {
+	AutoImage,
+	Image,
+	ImageCaption,
+	ImageInsert,
+	ImageResize,
+	ImageStyle,
+	ImageToolbar,
+	ImageUpload,
+	PictureEditing,
+} from '@ckeditor/ckeditor5-image';
+import { Indent, IndentBlock } from '@ckeditor/ckeditor5-indent';
+import { Link, AutoLink, LinkImage } from '@ckeditor/ckeditor5-link';
+import { List, ListProperties, TodoList } from '@ckeditor/ckeditor5-list';
+import { MediaEmbed, MediaEmbedToolbar } from '@ckeditor/ckeditor5-media-embed';
 import { Paragraph } from '@ckeditor/ckeditor5-paragraph';
 import { PasteFromOffice } from '@ckeditor/ckeditor5-paste-from-office';
-import { Table, TableToolbar } from '@ckeditor/ckeditor5-table';
+import {
+	Table,
+	TableCellProperties,
+	TableProperties,
+	TableToolbar,
+} from '@ckeditor/ckeditor5-table';
 import { TextTransformation } from '@ckeditor/ckeditor5-typing';
 import { CloudServices } from '@ckeditor/ckeditor5-cloud-services';
+import { Alignment } from '@ckeditor/ckeditor5-alignment';
+import { CodeBlock } from '@ckeditor/ckeditor5-code-block';
+import {
+	SpecialCharacters,
+	SpecialCharactersArrows,
+	SpecialCharactersCurrency,
+	SpecialCharactersEssentials,
+	SpecialCharactersLatin,
+	SpecialCharactersMathematical,
+	SpecialCharactersText,
+} from '@ckeditor/ckeditor5-special-characters';
+import { WordCount } from '@ckeditor/ckeditor5-word-count';
+import { RemoveFormat } from '@ckeditor/ckeditor5-remove-format';
+import { PageBreak } from '@ckeditor/ckeditor5-page-break';
+import { Mention } from '@ckeditor/ckeditor5-mention';
+import { HtmlEmbed } from '@ckeditor/ckeditor5-html-embed';
+import { HorizontalLine } from '@ckeditor/ckeditor5-horizontal-line';
+import { Highlight } from '@ckeditor/ckeditor5-highlight';
+import {
+	FontBackgroundColor,
+	FontColor,
+	FontFamily,
+	FontSize,
+} from '@ckeditor/ckeditor5-font';
+import { ExportPdf } from '@ckeditor/ckeditor5-export-pdf';
+import { ExportWord } from '@ckeditor/ckeditor5-export-word';
 
 export default class ClassicEditor extends ClassicEditorBase {
 	public static override builtinPlugins = [
 		Essentials,
 		UploadAdapter,
+		Alignment,
 		Autoformat,
+		AutoImage,
+		AutoLink,
 		Bold,
 		Italic,
 		BlockQuote,
 		CKBox,
 		CKFinder,
 		CloudServices,
+		Code,
+		CodeBlock,
 		EasyImage,
+		ExportPdf,
+		ExportWord,
+		FontBackgroundColor,
+		FontColor,
+		FontFamily,
+		FontSize,
 		Heading,
+		Highlight,
+		HorizontalLine,
+		HtmlEmbed,
 		Image,
 		ImageCaption,
+		ImageInsert,
+		ImageResize,
 		ImageStyle,
 		ImageToolbar,
 		ImageUpload,
 		Indent,
+		IndentBlock,
 		Link,
+		LinkImage,
 		List,
+		ListProperties,
 		MediaEmbed,
+		MediaEmbedToolbar,
+		Mention,
+		PageBreak,
 		Paragraph,
 		PasteFromOffice,
 		PictureEditing,
+		RemoveFormat,
+		SpecialCharacters,
+		SpecialCharactersArrows,
+		SpecialCharactersCurrency,
+		SpecialCharactersEssentials,
+		SpecialCharactersLatin,
+		SpecialCharactersMathematical,
+		SpecialCharactersText,
+		Strikethrough,
+		Subscript,
+		Superscript,
 		Table,
+		TableCellProperties,
+		TableProperties,
 		TableToolbar,
-		TextTransformation
+		TextTransformation,
+		Title,
+		TodoList,
+		Underline,
+		WordCount,
 	];
 
 	public static override defaultConfig = {
 		toolbar: {
 			items: [
-				'undo', 'redo',
-				'|', 'heading',
-				'|', 'bold', 'italic',
-				'|', 'link', 'uploadImage', 'insertTable', 'blockQuote', 'mediaEmbed',
-				'|', 'bulletedList', 'numberedList', 'outdent', 'indent'
-			]
+				'undo',
+				'redo',
+				'|',
+				'heading',
+				'|',
+				'alignment',
+				'bold',
+				'italic',
+				'underline',
+				'strikethrough',
+				'removeFormat',
+				'|',
+				'link',
+				'numberedList',
+				'bulletedList',
+				'blockQuote',
+				'code',
+				'codeBlock',
+				'|',
+				'horizontalLine',
+				'imageUpload',
+				'insertTable',
+				'specialCharacters',
+				'|',
+				'fontBackgroundColor',
+				'fontColor',
+				'fontSize',
+				'fontFamily',
+				'|',
+				'exportWord',
+				'exportPdf',
+			],
 		},
 		image: {
 			toolbar: [
@@ -73,17 +187,19 @@ export default class ClassicEditor extends ClassicEditorBase {
 				'imageStyle:side',
 				'|',
 				'toggleImageCaption',
-				'imageTextAlternative'
-			]
+				'imageTextAlternative',
+			],
 		},
 		table: {
 			contentToolbar: [
 				'tableColumn',
 				'tableRow',
-				'mergeTableCells'
-			]
+				'mergeTableCells',
+				'tableCellProperties',
+				'tableProperties',
+			],
 		},
 		// This value must be kept in sync with the language defined in webpack.config.js.
-		language: 'en'
+		language: 'en',
 	};
 }
